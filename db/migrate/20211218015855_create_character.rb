348c85fc244bdc71a192b3bf56ed1780b0c05f8a
Nicholas Davis <nickdavis1018@gmail.com>
2021-12-17 18:05:36 -0800

    Commit 4: Renamed the column

--- db/migrate/20211218015855_create_character.rb
+++ db/migrate/20211218015855_create_character.rb
@@ -1,8 +1,9 @@
 class CreateCharacter < ActiveRecord::Migration[6.1]
   def change
     create_table :characters do |t|
-
-      t.timestamps
+      t.string :name
+      t.string :desciption
+      t.string :img_url
     end
   end
 end
